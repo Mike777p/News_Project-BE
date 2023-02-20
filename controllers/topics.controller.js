1c30945071241da72bfa33b3b821a90af8cf3183
mike777p <mjp7@duck.com>
2023-02-20 16:51:57 +0000

    Add task 3 GET /api/topics/

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -4,10 +4,9 @@ const { handle500Status } = require("./ErrorHandlers");
 
 const getTopics = (request, response)=>{
     fetchTopics().then((topics)=>{
-        // console.log(topics)
         return response.status(200).send(topics)
     }).catch((error) => {
-        handle500Status(error, request, response)
+       
       });
 }
 
--- modules/topicsModule.js
+++ modules/topicsModule.js
@@ -1,5 +1,5 @@
 const db = require("../db/connection");
-// console.log(db.topicData)
+
 
 exports.fetchTopics = (()=>{
     return db.query(`SELECT * FROM topics`).then((data)=>{
