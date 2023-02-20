320dfa4e08dab0916bd622dff882b9f24004ed4b
mike777p <mjp7@duck.com>
2023-02-20 12:22:13 +0000

    Add setup files

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -2,3 +2,4 @@ exports.articleData = require('./articles.js');
 exports.commentData = require('./comments.js');
 exports.topicData = require('./topics.js');
 exports.userData = require('./users.js');
+
